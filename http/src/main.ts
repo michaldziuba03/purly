3323aea005e2134012e88beb7b4c4e1b93a24161
mich4ld <mchldziuba@gmail.com>
2021-10-06 00:43:45 +0200

    configure json errors in gateway

--- http/src/main.ts
+++ http/src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 
 // middlewares:
 import * as helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+  });
+
+  app.set('trust proxy');
   app.use(helmet());
   await app.listen(3000);
 }
