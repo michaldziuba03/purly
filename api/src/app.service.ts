96e2a8b2ee60f0d955d273968274e8ff5877ea87
mich4ld <mchldziuba@gmail.com>
2022-12-15 03:16:50 +0100

    init client application

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import Hashids from 'hashids';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly hashids = new Hashids('655deb');
+
+  createAlias() {
+    const alias = this.hashids.encode(50000);
+    
+    return { alias, url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' }
   }
 }
