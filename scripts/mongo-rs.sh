2aa043b0f954fda068209367e22dee5feb2096fd
mich4ld <mchldziuba@gmail.com>
2023-01-20 04:00:44 +0100

    build: create mongo replica set with single node

--- scripts/mongo-rs.sh
+++ scripts/mongo-rs.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-sleep 5
-mongosh <<EOF
-var config = {
-    "_id": "rs",
-    "version": 1,
-    "members": [
-        {
-            "_id": 1,
-            "host": "127.0.0.1:27017"
-        },
-        {
-            "_id": 2,
-            "host": "127.0.0.1:27018"
-        },
-        {
-            "_id": 3,
-            "host": "127.0.0.1:27019"
-        }
-    ]
-};
-rs.initiate(config);
-rs.status();
-EOF
+(sleep 10 && mongosh --host localhost --port 27017 --eval "rs.initiate()") &
+mongod --bind_ip localhost,mongo1 --replSet rs
