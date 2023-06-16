d41725b40599343a9daa65354d0b834d7a56be92
Michał Dziuba <mail@michaldziuba.dev>
2023-06-16 16:17:21 +0200

    feat(api): add stripe webhook base

--- apps/api/src/billing/billing.module.ts
+++ apps/api/src/billing/billing.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
 import { ManageSubscriptions } from './usecases/manage-subscriptions.usecase';
 import { CreateCheckout } from './usecases/create-checkout.usecase';
+import { StripeProvider } from './stripe/stripe.provider';
+import { StripeWebhook } from './stripe/stripe.webhook';
 
 @Module({
-  controllers: [BillingController],
-  providers: [ManageSubscriptions, CreateCheckout],
+  controllers: [BillingController, StripeWebhook],
+  providers: [StripeProvider, ManageSubscriptions, CreateCheckout],
 })
 export class BillingModule {}
---
+++ apps/api/src/billing/stripe/stripe.webhook.ts
@@ -0,0 +1,67 @@
+import {
+  Controller,
+  Post,
+  RawBodyRequest,
+  Headers,
+  Req,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectStripe } from './stripe.provider';
+import Stripe from 'stripe';
+
+export enum StripeEvents {
+  DeleteSubscription = 'customer.subscription.deleted',
+  UpdateSubscription = 'customer.subscription.updated',
+}
+
+@Controller('stripe/webhook')
+export class StripeWebhook {
+  constructor(
+    @InjectStripe()
+    private readonly stripe: Stripe
+  ) {}
+
+  private readonly relevantEvents = Object.values(StripeEvents);
+
+  // webhook path is optional variable for webhook url obfuscation
+  @Post(process.env.STRIPE_WEBHOOK_PATH)
+  async webhook(
+    @Req() req: RawBodyRequest<unknown>,
+    @Headers('stripe-signature') signature?: string
+  ) {
+    if (!process.env.STRIPE_WEBHOOK_SECRET) {
+      throw new NotFoundException();
+    }
+
+    if (!signature) {
+      throw new BadRequestException('Missing signature');
+    }
+
+    const evt = this.constructEvent(req.rawBody, signature);
+    if (!evt) {
+      throw new BadRequestException('Invalid event or signature');
+    }
+
+    // just ignore irrelevant events and return 2xx status
+    if (!this.relevantEvents.includes(evt.type as StripeEvents)) {
+      return;
+    }
+
+    // Handle event :)
+
+    return;
+  }
+
+  private constructEvent(payload: Buffer, signature: string) {
+    try {
+      return this.stripe.webhooks.constructEvent(
+        payload,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      return;
+    }
+  }
+}
