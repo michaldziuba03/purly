a3bf5ecbe8111bce3cb9de4881d8590634df23d5
Michał Dziuba <mail@michaldziuba.dev>
2023-07-04 23:57:45 +0200

    refactor(api): utm builder

--- apps/api/src/link/usecases/redirect-link.usecase.ts
+++ apps/api/src/link/usecases/redirect-link.usecase.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { isAfter } from 'date-fns';
 import { Usecase } from '../../shared/base.usecase';
 import { GetLink } from './get-link.usecase';
-import { DetectDevice, OS } from '../../shared/detect-device';
+import { DetectDevice } from '../../shared/detect-device';
+import { UtmBuilder } from '../utm.builder';
 
 interface IRedirectLinkCommand {
   alias: string;
@@ -34,26 +35,22 @@ export class RedirectLink implements Usecase<IRedirectLinkCommand> {
 
     const device = new DetectDevice(command.userAgent, command.remoteAddress);
 
-    if (link.androidRedirect && device.os === OS.ANDROID) {
+    if (link.enableUtm) {
+      return new UtmBuilder(link.url).buildLink({
+        utmCampaign: link.utmCampaign,
+        utmContent: link.utmContent,
+        utmMedium: link.utmMedium,
+        utmSource: link.utmSource,
+        utmTerm: link.utmTerm,
+      });
+    }
+
+    if (link.androidRedirect && device.isAndroid) {
       return link.androidRedirect;
     }
 
-    if (link.iosRedirect && device.os === OS.IOS) {
+    if (link.iosRedirect && device.isIOS) {
       return link.iosRedirect;
-    }
-
-    if (link.enableUtm) {
-      const utmUrl = new URL(link.url);
-
-      link.utmCampaign &&
-        utmUrl.searchParams.set('utm_campaign', link.utmCampaign);
-      link.utmContent &&
-        utmUrl.searchParams.set('utm_content', link.utmContent);
-      link.utmMedium && utmUrl.searchParams.set('utm_medium', link.utmMedium);
-      link.utmSource && utmUrl.searchParams.set('utm_source', link.utmSource);
-      link.utmTerm && utmUrl.searchParams.set('utm_term', link.utmTerm);
-
-      return utmUrl.toString();
     }
 
     return link.url;
