6de28d6e9476a9d2898794e251535a212cccf524
Michał Dziuba <mail@michaldziuba.dev>
2023-08-02 14:39:03 +0200

    refactor(api): simplify stats module

--- apps/api/src/stats/stats.controller.ts
+++ apps/api/src/stats/stats.controller.ts
@@ -5,46 +5,38 @@ import {
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { GetTopBrowsers } from './usecases/get-top-browsers.usecase';
 import { MembershipGuard } from '../workspace/framework/membership.guard';
 import { AuthenticatedGuard } from '../auth/guards/auth.guard';
 import { BaseController } from '../shared/base.controller';
 import { Membership } from '../workspace/framework/membership.decorator';
 import { Member } from '@purly/database';
-import { GetTopOS } from './usecases/get-top-os.usecase';
+import { AnalyticsService } from '@purly/analytics';
 import { StatsQueryDto } from './dto/stats-query.dto';
-import { GetTopCountries } from './usecases/get-top-countries.usecase';
-import { GetTopReferers } from './usecases/get-top-referers.usecase';
 
 @BaseController('stats')
 @UseGuards(AuthenticatedGuard, MembershipGuard)
 @UseInterceptors(ClassSerializerInterceptor)
 export class StatsController {
-  constructor(
-    private readonly getTopBrowsersUsecase: GetTopBrowsers,
-    private readonly getTopOSUsecase: GetTopOS,
-    private readonly getTopReferersUsecase: GetTopReferers,
-    private readonly getTopCountriesUsecase: GetTopCountries
-  ) {}
+  constructor(private readonly analyticsService: AnalyticsService) {}
 
   // TODO: add check if link exists by linkId and workspaceId in database
   @Get('browsers')
   topBrowsers(@Membership() member: Member, @Query() query: StatsQueryDto) {
-    return this.getTopBrowsersUsecase.execute({ linkId: query.linkId });
+    return this.analyticsService.getTopBrowsers(query.linkId);
   }
 
   @Get('os')
   topOS(@Query() query: StatsQueryDto) {
-    return this.getTopOSUsecase.execute({ linkId: query.linkId });
+    return this.analyticsService.getTopOS(query.linkId);
   }
 
   @Get('referers')
   topReferers(@Query() query: StatsQueryDto) {
-    return this.getTopReferersUsecase.execute({ linkId: query.linkId });
+    return this.analyticsService.getTopReferers(query.linkId);
   }
 
   @Get('countries')
   getTopCountries(@Query() query: StatsQueryDto) {
-    return this.getTopCountriesUsecase.execute({ linkId: query.linkId });
+    return this.analyticsService.getTopCountries(query.linkId);
   }
 }
